81d47b1e22ed13bff6311ad752d018b2b9a90860
Oleksandr Fedorenko <oleksandr.fedorenko@incrify.com>
2020-06-03 18:51:13 +0300

    Reset password

--- assets/js/component/page/authenticate/ResetPassword.js
+++ assets/js/component/page/authenticate/ResetPassword.js
@@ -3,8 +3,36 @@ import Button from "react-bootstrap/Button";
 import {bindActionCreators} from "redux";
 import {push} from "connected-react-router";
 import {connect} from "react-redux";
+import {validateForm} from "../../src/FormValidation";
+import {preloaderEnd, preloaderStart} from "../../src/Preloader";
+import axios from "axios";
+import {alert, alertException} from "../../src/Alert";
 
 function index(props) {
+    const [email, setEmail] = useState('');
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (!validateForm('reset-password-form')) {
+            return;
+        }
+
+        let formData = new FormData();
+        formData.set('User[email]', email);
+
+        preloaderStart();
+        axios.post('/react/reset-password', formData)
+            .then((res) => {
+                let data = res.data;
+                if (!data.status) {
+                    alert('error', data.error);
+                    return;
+                }
+                redirect(`/reset-password/email-send`)
+            })
+            .catch((error) => {alertException(error.response.status)})
+            .then(() => { preloaderEnd() });
+    };
 
     const redirect = (url) => {
         props.push(url);
@@ -15,9 +43,20 @@ function index(props) {
         <div className="reset-password container">
             <div className="col-xs-12 col-sm-6 col-md-4 block-center">
                 <div className={'block-reset'}>
-                    <form>
-                        <input className={'form-control input input-type-1 w-100'} placeholder={'Email'} type="email"/>
-                        <button type={"button"} className={"w-100 btn btn-type-2"} onClick={() => redirect('/reset-password/email-send')}>Восстановить</button>
+                    <span className={'block-name'}>Вход</span>
+                    <form className={'reset-password-form'} onSubmit={(e) => handleSubmit(e)} noValidate>
+
+                        <div className={`form-group`}>
+                            <input className={'form-control input input-type-1 w-100'}
+                                   placeholder={'Email'}
+                                   type="email"
+                                   value={email}
+                                   onChange={(e) => setEmail(e.target.value)}
+                                   required
+                            />
+                            <span className={'error'} />
+                        </div>
+                        <button type={"submit"} className={"w-100 btn btn-type-2"}>Восстановить</button>
                     </form>
                 </div>
             </div>
